292fb52d17a52194de09b6d26cca2fa7ce1fbbe5
np0307 <neha0307work@gmail.com>
2026-03-20 17:55:19 +0530

    Time: 0 ms (100.00%) | Memory: 41.9 MB (91.85%) - LeetSync

--- 319-bulb-switcher/bulb-switcher.java
+++ 319-bulb-switcher/bulb-switcher.java
@@ -1,6 +1,27 @@
 class Solution {
     public int bulbSwitch(int n) {
-       return (int)Math.sqrt(n);
+      if(n==0)
+      {
+        return 0;
+      }
+
+      int low = 1, high = n, ans=0;
+
+      while(low<=high)
+      {
+        int mid = low+(high-low)/2;
+
+        if((long)mid*mid<=n)
+        {
+            ans=mid;
+            low=mid+1;
+        }
+        else
+        {
+            high=mid-1;
+        }
+      }
+      return ans;
         
     }
 }
